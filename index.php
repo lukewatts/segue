4666d084a42c67809242693d38e3d132b4d1927d
Luke Watts <luke@luke-watts.com>
2014-05-15 12:01:07 +0100

    Add The Loop

--- index.php
+++ index.php
@@ -75,16 +75,38 @@
   <div class="container">
     <div class="row">
       <div class="span6 ctb">
-        <p>Lo-fi actually Vice, Williamsburg 8-bit pork belly twee fap Godard hella church-key. Leggings asymmetrical fashion axe craft beer beard, chillwave readymade thundercats Brooklyn 3 wolf moon Echo Park. Marfa art party post-ironic, tofu cred deep v meggings sustainable. Tote bag intelligentsia flexitarian, pickled salvia Pinterest brunch forage shabby chic.</p>
-        <p>Pug shoreditch Williamsburg, beard wolf cred polaroid. Direct trade master cleanse DIY, polaroid Wes Anderson McSweeney's tumblr wolf umami kale chips squid. Farm-to-table aesthetic blog, ethical seitan master cleanse photo booth 3 wolf moon polaroid blue bottle flannel. </p>
+        <!-- START OF "The Loop" -->
+        <?php
+        /**
+         * TODO: use the_ID() to generate a CSS class for each post
+         * TODO: Add metadat for author, time, categories, taxonomy etc
+         * TODO: Add moretag functionality
+         * TODO: Add "edit post" functionality
+         * TODO: Ensure scheduled posts don't appear before they're meant to
+         */
+        ?><?php if( have_posts() ) : ?>
+
+          <?php while( have_posts() ) : the_post();  ?>
+
+          <article>
+            
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            
+            <?php the_content(); ?>
+            
+          </article>          
+
+            <?php endwhile; ?>
+
+          <?php endif; ?><!-- END OF "The Loop" -->
       
-        <div class="news">
+        <!-- <div class="news">
           <h3>Flavours of the week</h3>
           <a href = "lorempixel.com">
             <img src="<?php echo IMG; ?>placeholder.jpg" alt=""/>
           </a>
-        </div><!-- .news -->
-      </div><!-- .span6.ctb -->
+        </div> -->
+      </div><!-- .span6 -->
 
       <div class="span4 sidebar">
         <a href ="http://codepen.io">
